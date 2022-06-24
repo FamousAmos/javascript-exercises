b726e17ea8644684bdc9915e7e926a3ffabf571d
Amos Kenner <akenner92@gmail.com>
2022-06-24 15:15:10 -0500

    Solve palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  filteredString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+  return filteredString.split("").reverse().join("") == filteredString;
 };
 
 // Do not edit below this line
